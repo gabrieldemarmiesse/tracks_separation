c58b9b870ef007c5ab0cf0b22f832c918e9aa575
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2017-10-24 20:00:05 +0200

    Now the model padding is "valid".

--- keras_functions.py
+++ keras_functions.py
@@ -2,48 +2,66 @@ import keras
 import numpy as np
 from keras.models import Model
 from keras.layers import *
+import keras.backend as K
 
 
-def get_model(filters, nb_blocks=3, conv_per_block=8):
+def add_centered(list_of_tensors):
+    """ Sum a list of tensors of different sizes. The annoying part is
+        to be sure that they are centered when they are summed."""
 
-    print("last conv is dilated by a factor of", 2**(conv_per_block-1))
-    print("receptive field:", nb_blocks*(2**conv_per_block))
+    cst_2 = K.constant(2, dtype="int32")
+    result = list_of_tensors[-1]
+    smallest_shape = K.shape(result)[1]
+
+    for tensor in list_of_tensors[:-1]:
+        diff = K.shape(tensor)[1] - smallest_shape
+        print(K.dtype(diff))
+        pad = diff // cst_2
+        print(K.dtype(pad))
+        result += tensor[:, pad:-pad, :]
+
+    return result
+
+
+def get_generator_model(filters, nb_blocks=3, conv_per_block=8):
+    print("last conv is dilated by a factor of", 2 ** (conv_per_block - 1))
+    print("receptive field:", nb_blocks * (2 ** conv_per_block))
 
     def apply_conv(x, dilatation_rate):
-        tanh_out = Conv1D(filters, 2,
+        tanh_out = Conv1D(filters, 3,
                           dilation_rate=dilatation_rate,
-                          activation="tanh",
-                          padding="same")(x)
-        sigm_out = Conv1D(filters, 2,
+                          activation="tanh")(x)
+        sigm_out = Conv1D(filters, 3,
                           dilation_rate=dilatation_rate,
-                          activation="sigmoid",
-                          padding="same")(x)
+                          activation="sigmoid")(x)
 
         return Multiply()([tanh_out, sigm_out])
 
     def residual_block(x, dilatation_rate):
         out_act = apply_conv(x, dilatation_rate)
 
-        res_x = Conv1D(filters, 1, padding="same")(out_act)
-        res_x = Add()([x, res_x])
+        res_x = Conv1D(filters, 1)(out_act)
+        res_x = Lambda(add_centered, lambda x: x[-1])([x, res_x])
 
-        skip_x = Conv1D(filters, 1, padding="same")(out_act)
+        skip_x = Conv1D(filters, 1)(out_act)
         return res_x, skip_x
-
 
     input_tensor = Input(shape=(None, 1), name='input_part')
 
-    x=input_tensor
+    x = input_tensor
     skip_connections = []
 
     for i in range(nb_blocks):
         for j in range(conv_per_block):
-            x, skip_x = residual_block(x, 2**j)
+            x, skip_x = residual_block(x, 2 ** j)
             skip_connections.append(skip_x)
 
-    out = Add()(skip_connections)
+    out = Lambda(add_centered, lambda x: x[-1])(skip_connections)
     out = Activation("relu")(out)
-    out = Conv1D(filters, 3, padding="same", activation="relu")(out)
-    out = Conv1D(1, 1, padding="same", activation="linear")(out)
+    out = Conv1D(filters, 3, activation="relu")(out)
+    out = Conv1D(1, 1, activation="linear")(out)
     model = Model(input_tensor, out)
+
+    print(model.layers[-1])
+
     return model
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.io.wavfile import read, write
 from resampy import resample
 from time import time
-from keras_functions import get_model
+from keras_functions import get_generator_model
 
 
 def preprocess(array):
@@ -35,13 +35,21 @@ in_data = preprocess(in_data)
 out_data = in_data_vocals
 out_data = preprocess(out_data)
 
-model = get_model(256, 2, 2)
+model = get_generator_model(256, 2, 2)
+
+dummy_size = 30000
+dummy_input = np.zeros((1,dummy_size,1))
+padding = (dummy_size - model.predict(dummy_input).shape[1])//2
+print(padding)
+
+
 
 for i in range(out_data.shape[1]):
     if out_data[0,i,0] !=0:
         break
+print(i)
 model.compile("adam", "mae")
 print(np.sum(np.abs(out_data)))
 print(np.max(np.abs(out_data)))
-model.fit(in_data[:,i:i+20000], out_data[:,i:i+20000], 1, 2)
+model.fit(in_data[:,i:i+20000], out_data[:,i+padding:i+20000-padding], 1, 2)
 model.save("./data/models/first_model.h5")
