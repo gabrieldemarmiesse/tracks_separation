40e54c512d39c13124fcdf62baa3898762f4ef43
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2017-10-26 23:40:34 +0200

    Full model made and training procedure done.

--- keras_functions.py
+++ keras_functions.py
@@ -23,28 +23,32 @@ def add_centered(list_of_tensors):
     return result
 
 
-def get_generator_model(filters, nb_blocks=3, conv_per_block=8):
+def apply_conv(x, dilatation_rate, filters, padding="valid"):
+    tanh_out = Conv1D(filters, 3,
+                      dilation_rate=dilatation_rate,
+                      activation="tanh",
+                      padding=padding)(x)
+    sigm_out = Conv1D(filters, 3,
+                      dilation_rate=dilatation_rate,
+                      activation="sigmoid",
+                      padding=padding)(x)
+
+    return Multiply()([tanh_out, sigm_out])
+
+
+def residual_block(x, dilatation_rate, filters, padding="valid"):
+    out_act = apply_conv(x, dilatation_rate)
+
+    res_x = Conv1D(filters, 1, padding=padding)(out_act)
+    res_x = Lambda(add_centered, lambda x: x[-1])([x, res_x])
+
+    skip_x = Conv1D(filters, 1, padding=padding)(out_act)
+    return res_x, skip_x
+
+
+def get_generator(filters, nb_blocks=3, conv_per_block=8):
     print("last conv is dilated by a factor of", 2 ** (conv_per_block - 1))
     print("receptive field:", nb_blocks * (2 ** conv_per_block))
-
-    def apply_conv(x, dilatation_rate):
-        tanh_out = Conv1D(filters, 3,
-                          dilation_rate=dilatation_rate,
-                          activation="tanh")(x)
-        sigm_out = Conv1D(filters, 3,
-                          dilation_rate=dilatation_rate,
-                          activation="sigmoid")(x)
-
-        return Multiply()([tanh_out, sigm_out])
-
-    def residual_block(x, dilatation_rate):
-        out_act = apply_conv(x, dilatation_rate)
-
-        res_x = Conv1D(filters, 1)(out_act)
-        res_x = Lambda(add_centered, lambda x: x[-1])([x, res_x])
-
-        skip_x = Conv1D(filters, 1)(out_act)
-        return res_x, skip_x
 
     input_tensor = Input(shape=(None, 1), name='input_part')
 
@@ -53,7 +57,7 @@ def get_generator_model(filters, nb_blocks=3, conv_per_block=8):
 
     for i in range(nb_blocks):
         for j in range(conv_per_block):
-            x, skip_x = residual_block(x, 2 ** j)
+            x, skip_x = residual_block(x, 2 ** j, filters)
             skip_connections.append(skip_x)
 
     out = Lambda(add_centered, lambda x: x[-1])(skip_connections)
@@ -65,3 +69,59 @@ def get_generator_model(filters, nb_blocks=3, conv_per_block=8):
     print(model.layers[-1])
 
     return model
+
+
+def average_all(tensor):
+    x = K.mean(tensor, axis=(1, 2))
+    return K.expand_dims(x, 1)
+
+
+def average_layer(tensor):
+    return Lambda(average_all, lambda x: (x[0], 1))(tensor)
+
+
+def get_discriminator(filters, nb_blocks=3, conv_per_block=8):
+    print("last conv is dilated by a factor of", 2 ** (conv_per_block - 1))
+    print("receptive field:", nb_blocks * (2 ** conv_per_block))
+
+    input_0 = Input(shape=(None, 1), name='input_0')
+    input_1 = Input(shape=(None, 1), name='input_1')
+
+    x = input_0
+    skip_connections_0 = []
+
+    for i in range(nb_blocks):
+        for j in range(conv_per_block):
+            x, skip_x = residual_block(x, 2 ** j, filters, padding="same")
+            skip_connections_0.append(skip_x)
+
+    features_extractor = Model(input_0, skip_connections_0)
+
+    skip_connections_1 = features_extractor(input_1)
+
+    out_0 = [average_layer(a) for a in skip_connections_0]
+    out_0 = Average()(out_0)
+
+    out_1 = [average_layer(a) for a in skip_connections_1]
+    out_1 = Average()(out_1)
+
+    out_sub = Subtract()([out_0, out_1])
+    out = Activation("sigmoid")(out_sub)
+    return Model([input_0, input_1], out)
+
+
+def get_full_model(filters, nb_blocks, conv_per_block):
+    generator = get_generator(filters, nb_blocks, conv_per_block)
+    discriminator = get_discriminator(filters, nb_blocks, conv_per_block)
+
+    # We need two inputs
+    input_mixed = Input(shape=(None, 1), name='input_mixed')
+
+    input_true_voice = Input(shape=(None, 1), name='input_true_voice')
+
+    predicted_voice = generator(input_mixed)
+
+    predicted_original = discriminator([predicted_voice, input_true_voice])
+
+    full_model = Model([input_mixed, input_true_voice], [predicted_voice, predicted_original])
+    return full_model, generator, discriminator
--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import numpy as np
 from scipy.io.wavfile import read, write
 from resampy import resample
 from time import time
-from keras_functions import get_generator_model
+from keras_functions import get_generator
+from keras.callbacks import TerminateOnNaN, ModelCheckpoint, TensorBoard
+
+nb_steps_MAE = 10000
+nb_steps_GAN = 10000
 
 
 def preprocess(array):
@@ -11,15 +15,25 @@ def preprocess(array):
         array = np.expand_dims(array, 0)
     new_array = np.sum(array, axis=2, keepdims=True)
     new_array = np.broadcast_to(new_array, (10, new_array.shape[1], 1))
-    return new_array/(2**14)
+    return new_array / (2 ** 14)
 
 
 def postprocess(array):
     array = array[0]
-    array *= 2**14
-    array = np.clip(array, -(2**14), 2**14)
+    array *= 2 ** 14
+    array = np.clip(array, -(2 ** 14), 2 ** 14)
     array = array.astype(np.int16)
     return np.broadcast_to(array, (array.shape[0], 2))
+
+
+def training_procedure(full_model, generator, discriminator, data_generator_train, data_generator_val):
+
+
+    # Callbacks:
+
+
+    # We need to first train the generator, using only MAE, then, we move on to using the discriminator too.
+    generator.fit_generator(data_generator_train, 1000, epochs=10, validation_data=data_generator_val)
 
 
 folder = "./data/DSD100_16kHz/Sources/Dev/051 - AM Contra - Heart Peripheral/"
@@ -35,21 +49,19 @@ in_data = preprocess(in_data)
 out_data = in_data_vocals
 out_data = preprocess(out_data)
 
-model = get_generator_model(256, 2, 2)
+model = get_generator(256, 2, 2)
 
 dummy_size = 30000
-dummy_input = np.zeros((1,dummy_size,1))
-padding = (dummy_size - model.predict(dummy_input).shape[1])//2
+dummy_input = np.zeros((1, dummy_size, 1))
+padding = (dummy_size - model.predict(dummy_input).shape[1]) // 2
 print(padding)
 
-
-
 for i in range(out_data.shape[1]):
-    if out_data[0,i,0] !=0:
+    if out_data[0, i, 0] != 0:
         break
 print(i)
 model.compile("adam", "mae")
 print(np.sum(np.abs(out_data)))
 print(np.max(np.abs(out_data)))
-model.fit(in_data[:,i:i+20000], out_data[:,i+padding:i+20000-padding], 1, 2)
+model.fit(in_data[:, i:i + 20000], out_data[:, i + padding:i + 20000 - padding], 1, 2)
 model.save("./data/models/first_model.h5")
